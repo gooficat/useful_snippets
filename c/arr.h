0364533667f546f9cec0506509b5b6db20e3f349
Hasan Sabri <84398988+gooficat@users.noreply.github.com>
2025-12-02 16:02:48 +0000

    Create arr.h

---
+++ c/arr.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "defs.h"
+#include <malloc.h>
+#include <stdlib.h>
+
+struct generic_arr
+{
+	u64 len;
+	u64 powmul;
+	void ptr get;
+};
+
+#define array_type(type) struct {\
+    u64 len;\
+    u64 powmul;\
+    type ptr get;\
+}
+
+typedef array_type ( str ) strarr_t;
+
+#define mk_arr(t) {\
+    .get = malloc(sizeof(t)),\
+    .len = 0,\
+    .powmul = 1\
+}
+
+#define del_arr(arr) free(arr.get)
+
+#define at(i) get[i]
+
+strarr_t t;
+
+
+void rsz_arr_f ( struct generic_arr ptr arr , u8 sz );
+
+
+#define rsz_arr(arr, type, amt) arr.len += amt; rsz_arr_f((struct generic_arr ptr)addr arr, sizeof(type))
