01c7af5adcf1bb2cd7ad1ffd9c0d21707150b57c
Hasan Sabri <84398988+gooficat@users.noreply.github.com>
2025-12-15 15:46:06 +0000

    Update defs.h

--- c/defs.h
+++ c/defs.h
@@ -23,23 +23,32 @@ typedef signed char i8;
 typedef unsigned char u8;
 
 #ifdef _WIN32
-typedef signed short i16;
-typedef signed long i32;
-typedef signed long long i64;
-
-typedef unsigned short u16;
-typedef unsigned long u32;
-typedef unsigned long long u64;
+  typedef signed short i16;
+  typedef signed long i32;
+  typedef signed long long i64;
+  
+  typedef unsigned short u16;
+  typedef unsigned long u32;
+  typedef unsigned long long u64;
+  
+  #ifdef _WIN64
+    #ifdef __GNUC__
+      typedef signed int i128 __attribute__((mode(TI)));
+      typedef unsigned int u128 __attribute__((mode(TI)));
+    #endif
+  #endif
 #else
-typedef signed short i16;
-typedef signed int i32;
-typedef signed long i64;
-typedef signed long long i128;
-
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef unsigned long u64;
-typedef unsigned long long u128;
+    typedef signed short i16;
+    typedef signed int i32;
+    typedef signed long i64;
+    
+    typedef unsigned short u16;
+    typedef unsigned int u32;
+    typedef unsigned long u64;
+    #ifdef __X86_64__
+      typedef signed long long i128;
+      typedef unsigned long long u128;
+    #endif
 #endif
 
 typedef char ptr str;
